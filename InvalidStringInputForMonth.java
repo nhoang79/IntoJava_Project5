0db788f7e519ffb61f89e7569289705b12f22e11
Aurora N Hoang <37682350+nhoang79@users.noreply.github.com>
2019-03-04 11:14:48 -0800

    Create InvalidStringInputForMonth.java

---
+++ InvalidStringInputForMonth.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * @author Ngan
+ */
+public class InvalidStringInputForMonth extends Exception {
+
+    /**
+     * Creates a new instance of <code>InvalidStringInputForMonth</code> without
+     * detail message.
+     */
+    public InvalidStringInputForMonth() {
+    }
+
+    /**
+     * Constructs an instance of <code>InvalidStringInputForMonth</code> with
+     * the specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public InvalidStringInputForMonth(String msg) {
+        super(msg);
+    }
+}
