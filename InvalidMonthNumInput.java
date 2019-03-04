bdda4f5d59ddd18265a2d65c3f9c0c8e028e9caa
Aurora N Hoang <37682350+nhoang79@users.noreply.github.com>
2019-03-04 11:15:07 -0800

    Create InvalidMonthNumInput.java

---
+++ InvalidMonthNumInput.java
@@ -0,0 +1,24 @@
+/*
+/**
+ *
+ * @author Ngan
+ */
+public class InvalidMonthNumInput extends Exception {
+
+    /**
+     * Creates a new instance of <code>InvalidMonthNumInput</code> without
+     * detail message.
+     */
+    public InvalidMonthNumInput() {
+    }
+
+    /**
+     * Constructs an instance of <code>InvalidMonthNumInput</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public InvalidMonthNumInput(String msg) {
+        super(msg);
+    }
+}
